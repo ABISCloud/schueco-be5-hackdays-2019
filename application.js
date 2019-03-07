fbc157b45ad74f008e109420b2b5336c37b21d7c
daniel <daniel.janz@dbi-analytics.de>
2019-03-07 03:57:50 +0100

    adopted to PZM data source

--- application.js
+++ application.js
@@ -9,7 +9,7 @@ let messages = {
     "rel_humidity":{
 
     },
-    "ambient_temperature":{
+    "moisture_level":{
 
     },
     "noise":{
@@ -53,9 +53,9 @@ $(() => {
         updateSensorDropDown();
     };
 
-    let tempChart = null;
+    let moistureChart = null;
     let humidChart = null;
-    let windspeedChart = null;
+    let tempChart = null;
 
     let notificationSensorStatus = "1";
     let indicatorEl = document.getElementsByClassName('liveIndicator')[0];
@@ -64,8 +64,8 @@ $(() => {
         updateSensorDropDown();
 
         setTimeout(() => {
-            if (!tempChart) {
-                tempChart = new Chart(property.sensors['ambient_temperature'], (notificationSensor, diff) => {
+            if (!moistureChart) {
+                moistureChart = new Chart(property.sensors['moisture_level'], (notificationSensor, diff) => {
                     // alert(notificationSensor.name + " reported an issue");
 
                     if (diff < 0) {
@@ -74,52 +74,33 @@ $(() => {
                         notificationSensorStatus = '3';
                     }
 
-                    switch(notificationSensorStatus) {
-                        case '1':
-                            Array.from(indicatorEl.classList.entries()).forEach(o => indicatorEl.classList.remove(o));
-                            indicatorEl.classList.add('liveIndicator');
-                            indicatorEl.classList.add('greenGood');
-                        break;
-                        case '2':
-                            Array.from(indicatorEl.classList.entries()).forEach(o => indicatorEl.classList.remove(o));
-                            indicatorEl.classList.add('liveIndicator');
-                            indicatorEl.classList.add('yellowModarte');
-                        break;
-                        case '3':
-                            Array.from(indicatorEl.classList.entries()).forEach(o => indicatorEl.classList.remove(o));
-                            indicatorEl.classList.add('liveIndicator');
-                            indicatorEl.classList.add('criticalRed');
-                        break;
-                        default:
-                            Array.from(indicatorEl.classList.entries()).forEach(o => indicatorEl.classList.remove(o));
-                            indicatorEl.classList.add('liveIndicator');
-                            indicatorEl.classList.add('criticalRed');
-                            break;
-                    }
                     $(".panel-collapse").collapse("show");
 
-
                     setTimeout(() => {
-
-
                         let mapframe = document.getElementById('mapframe');
                         mapframe.src = "https://dbianalytics.github.io/schueco-be5-hackdays-2019/bim/examples/gltf.html";
 
                         //selectionChanged(window.bimSurfer, '47#product-6b61ce71-1a7a-473c-8f87-4262e0bdb448-body.entity.0.0', Utils);
                     }, 5000)
                 });
-                tempChart.attach(chartArea.id);
+                moistureChart.attach(chartArea.id);
             }
 
             if (!humidChart) {
-                humidChart = new Chart(property.sensors['userdefined_double_2'], null, 'rel_humidity');
+                humidChart = new Chart(property.sensors['light'], null, 'light');
                 humidChart.attach(chartArea.id);
             }
 
+            if (!tempChart) {
+                tempChart = new Chart(property.sensors['temp'], null, 'temp');
+                tempChart.attach(chartArea.id);
+            }
+/*
             if (!windspeedChart) {
                 windspeedChart = new Chart(property.sensors['wind_speed']);
                 windspeedChart.attach(chartArea.id);
             }
+            */
         }, 75);
     });
 });
